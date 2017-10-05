c100d13795198e2a7f5500b599a467e0847dd50c
5802 <anawat@seniorsoft.co.th>
2017-10-05 12:16:10 +0700

    ExtensionMethod และ เพิ่มตัวอย่าง Predicate

--- DelegateLab/BasicPredicate/Program.cs
+++ DelegateLab/BasicPredicate/Program.cs
@@ -21,6 +21,26 @@ namespace Predicate
             Predicate<int> isEqualToA = i => i == A;
             result = isEqualToA(1);
             result = isEqualToA(100);
+
+            List<string> list1 = new List<string>();
+            list1.Add("1");
+            list1.Add("2");
+            list1.Add("3");
+            list1.Add("4");
+            List<string> list1result = list1.FindAll((string x) => x == "1" || x == "2");
+
+            List<Composite> list2 = new List<Composite>();
+            list2.Add(new Composite() { valueA = "1", valueB = "a" });
+            list2.Add(new Composite() { valueA = "2", valueB = "b" });
+            list2.Add(new Composite() { valueA = "3", valueB = "c" });
+            list2.Add(new Composite() { valueA = "4", valueB = "d" });
+            List<Composite> list2result = list2.FindAll((Composite x) => x.valueA == "1" || x.valueA == "2");
+        }
+
+        class Composite
+        {
+            public string valueA;
+            public string valueB;
         }
     }
 }
---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "1,000";
+            int value = input.ParseToInt();
+
+            ConsoleKey
+            consoleKey = "A".ParseToEnumConsoleKey();
+            consoleKey = "B".ParseToEnumConsoleKey();
+            consoleKey = "Backspace".ParseToEnumConsoleKey();
+        }
+    }
+
+    //ต้องประกาศ static class ไว้ชั้นนอกสุด อยู่ใต้แค่ namespace เท่านั้น
+    static class ExtensionMethods
+    {
+        public static int ParseToInt(this string value)
+        //     ^^^^^^                ^^^^^^^^^^^
+        //  จุดสำคัญคือตรงนี้           จุดสำคัญคือตรงนี้
+        //  ต้องเป็น static          ต้องมีคำว่า this และ type string หลังจาก this หมายถึงว่าจะให้ method นี้ไปเป็น extension method ของ type ไหน
+        //เวลาใช้งานจะเป็นแบบนี้
+        //    string input = "1,000";
+        //    int value = input.ParseToInt();
+        //ไม่ใช่แบบนี้
+        //    string.ParseToInt("1,000");
+        {
+            return int.Parse(value, System.Globalization.NumberStyles.AllowThousands);
+        }
+
+        public static ConsoleKey ParseToEnumConsoleKey(this string consoleKey)
+        {
+            switch (consoleKey)
+            {
+                case "A":
+                    return ConsoleKey.A;
+                case "B":
+                    return ConsoleKey.B;
+                default:
+                    return (ConsoleKey)Enum.Parse(typeof(ConsoleKey), consoleKey);
+            }
+        }
+    }
+}

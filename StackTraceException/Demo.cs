98c61128c3a326990d968217b751370b4a64cbf4
5802 <anawat@seniorsoft.co.th>
2017-11-16 10:27:01 +0700

    Exception Rethrowing

---
+++ StackTraceException/Demo.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StackTraceException
+{
+    /// <summary>
+    /// ตัวอย่างการ Catch แล้ว Rethrow แบบไม่เสีย StackTrace
+    /// </summary>
+    class Demo
+    {
+        public static void work()
+        {
+            try
+            {
+                lv1 _lv1 = new lv1();
+                _lv1.work1();
+
+                throw new Exception("main");
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        class lv1
+        {
+            public void work1()
+            {
+                try
+                {
+                    lv2 _lv2 = new lv2();
+                    _lv2.work2();
+
+                    throw new Exception("work1");
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+
+            class lv2
+            {
+                public void work2()
+                {
+                    try
+                    {
+                        lv3 _lv3 = new lv3();
+                        _lv3.work3();
+
+                        throw new Exception("work2");
+                    }
+                    catch (Exception e)
+                    {
+                        throw;
+                    }
+                }
+
+                class lv3
+                {
+                    public void work3()
+                    {
+                        try
+                        {
+                            lv4 _lv4 = new lv4();
+                            _lv4.work4();
+
+                            throw new Exception("work3");
+                        }
+                        catch (Exception e)
+                        {
+                            throw;
+                        }
+                    }
+
+                    class lv4
+                    {
+                        public void work4()
+                        {
+                            try
+                            {
+                                throw new Exception("work4");
+                            }
+                            catch (Exception e)
+                            {
+                                throw;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ StackTraceException/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StackTraceException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //this one should've been the best way to write as it's the most robust & clean one, but in practice it doesn't
+                //as this one lost the vital stack trace during the catch throw process
+                SampleLogic.LogicA();
+            }
+            catch (Exception e)
+            {
+            }
+
+            try
+            {
+                //this one is only just an acceptable way to write as it's not even clean way to write, but in practice it actually is the best way to write
+                //as this one is capable of preserve the vital stack trace during the catch throw process
+                SampleLogic.LogicB();
+            }
+            catch (Exception e)
+            {
+            }
+
+            try
+            {
+                //this one should've been the worst way to write as it's so ugly way of exception handling, but in practice it actually is on par with LogicB in result
+                //as this one is capable of preserve the vital stack trace during the catch throw process
+                SampleLogic.LogicC();
+            }
+            catch (Exception e)
+            {
+            }
+
+            try
+            {
+                //this one does not handle any exception at all, although crude but this one is capable of preserve the vital stack trace during the catch throw process
+                //and might've been the best way to go for release build as catching and rethrow is helpful in debugging but are completely useless in production environment
+                SampleLogic.LogicD();
+            }
+            catch (Exception e)
+            {
+            }
+
+            try
+            {
+                //An example of LogicB style in practice
+                Demo.work();
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+
+    class SampleLogic
+    {
+        /// <summary>
+        /// Robust & Clean Code with explicitly stated 'catch (Exception e)' and 'throw e'
+        /// </summary>
+        public static void LogicA()
+        {
+            try
+            {
+                InternalLogic();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Lazy Code with explicitly stated 'catch (Exception e)' and ambiguously stated 'throw'
+        /// </summary>
+        public static void LogicB()
+        {
+            try
+            {
+                InternalLogic();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Extremely Lazy Code with ambiguously stated 'catch' and 'throw'
+        /// </summary>
+        public static void LogicC()
+        {
+            try
+            {
+                InternalLogic();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// No catch and throw at all
+        /// </summary>
+        public static void LogicD()
+        {
+            InternalLogic();
+        }
+
+        static void InternalLogic()
+        {
+            int a = 0;
+            int b = 10 / a;
+        }
+    }
+}

b9f89041f3194d573f007317d67e66035a5fa1da
5802 <anawat@seniorsoft.co.th>
2018-02-06 11:01:50 +0700

    NamedTuple

---
+++ NamedTuple/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedTuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string A, string B, string C) TupleWithName(string a, string b, string c) // tuple return type
+            {
+                a += a;
+                b += b;
+                c += c;
+                return (A: a, B: b, C: c); // tuple literal
+            }
+
+            var tuple = TupleWithName("aaa", "bbb", "ccc");
+            string aa = tuple.A;
+            string bb = tuple.B;
+            string cc = tuple.C;
+        }
+    }
+}

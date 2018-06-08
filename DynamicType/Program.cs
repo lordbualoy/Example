d0f771a31b840d828506f7fe15e2f13b33c990f1
Malchazeen <malchazeen.edge@gmail.com>
2018-06-09 01:00:24 +0700

    DynamicType

---
+++ DynamicType/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic dynamic = new System.Dynamic.ExpandoObject();
+            dynamic.A = 10;
+            dynamic.B = "10";
+
+            int a = dynamic.A;
+            string b = dynamic.B;
+
+            //try to assign int to string; no compiler's error
+            try { b = dynamic.A; }
+            catch(Exception e) { }
+
+            //try to assign string to int; no compiler's error
+            try { a = dynamic.B; }
+            catch(Exception e) { }
+        }
+    }
+}

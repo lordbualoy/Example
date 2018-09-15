1f8a8b1387f73ddd61b74c7acd94ced553747bd5
Malchazeen <malchazeen.edge@gmail.com>
2018-09-15 18:42:23 +0700

    CommonSortingAlgorithm

---
+++ CommonSortingAlgorithm/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonSortingAlgorithm
+{
+    class Program
+    {
+        static int[] baseArray = new int[] { 4, 8, 7, 1, 3, 0, 5, 9, 6, 2 };
+
+        static void Main(string[] args)
+        {
+            BubbleSort();
+            SelectionSort();
+            InsertionSort();
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void BubbleSort()
+        {
+            int[] arr = new int[baseArray.Length];
+            Array.Copy(baseArray, arr, baseArray.Length);
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                        Swap(ref arr[j], ref arr[j + 1]);
+                }
+            }
+        }
+
+        static void SelectionSort()
+        {
+            int[] arr = new int[baseArray.Length];
+            Array.Copy(baseArray, arr, baseArray.Length);
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int indexOfMin = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[indexOfMin])
+                        indexOfMin = j;
+                }
+                if (indexOfMin != i)
+                    Swap(ref arr[i], ref arr[indexOfMin]);
+            }
+        }
+
+        static void InsertionSort()
+        {
+            int[] arr = new int[baseArray.Length];
+            Array.Copy(baseArray, arr, baseArray.Length);
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int temp = arr[i];
+                bool hasAnyValidMoves = false;
+                int j;
+                for (j = i; j > 0 && temp < arr[j - 1]; j--)
+                {
+                    arr[j] = arr[j - 1];
+                    hasAnyValidMoves = true;
+                }
+                if (hasAnyValidMoves)
+                    arr[j] = temp;
+            }
+        }
+    }
+}
---
+++ CommonSortingAlgorithm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CommonSortingAlgorithm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CommonSortingAlgorithm")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bb9531a1-15d5-4643-8d1e-6a9a5c81390a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

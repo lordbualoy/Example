135151be822c744a02d041f0d67d936f7bef6ee5
Malchazeen <malchazeen.edge@gmail.com>
2018-09-16 17:23:42 +0700

    ADO.Net

---
+++ ADO.Net_SqlCommand_Parameters/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADO.Net_SqlCommand_Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Experiment;Integrated Security=True;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string sql = "INSERT INTO TABLE1 VALUES (@Id, @Name);";
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        SqlParameter parameter;
+                        parameter = new SqlParameter("@Id", 5);
+                        cmd.Parameters.Add(parameter);
+                        parameter = new SqlParameter { ParameterName = "@Name", Value = "e", DbType = System.Data.DbType.AnsiString };
+                        cmd.Parameters.Add(parameter);
+
+                        connection.Open();
+                        cmd.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+            }
+            finally
+            {
+            }
+        }
+    }
+}
---
+++ ADO.Net_StoredProcedure/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADO.Net_StoredProcedure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            CREATE PROCEDURE [dbo].[Procedure]
+	            @p1 int,
+	            @p2 varchar(5)
+            AS
+	            insert into Table1 values(@p1, @p2)
+            */
+
+            try
+            {
+                string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Experiment;Integrated Security=True;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string procedureName = "Procedure";
+                    using (SqlCommand cmd = new SqlCommand(procedureName, connection))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        SqlParameter parameter;
+                        parameter = new SqlParameter("@p1", 6);
+                        cmd.Parameters.Add(parameter);
+                        parameter = new SqlParameter() { ParameterName = "@p2", Value = "f", DbType = System.Data.DbType.AnsiString };
+                        cmd.Parameters.Add(parameter);
+
+                        connection.Open();
+                        cmd.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+            }
+            finally
+            {
+            }
+        }
+    }
+}

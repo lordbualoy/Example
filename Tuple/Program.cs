792e181a0b7045adf8f4047c7c96eee58533c46d
anawat <a@a>
2018-10-16 15:45:20 +0700

    Merged Tuple and NamedTuple into single solution

--- Tuple/Program.cs
+++ Tuple/Program.cs
@@ -9,7 +9,22 @@ namespace Tuple
     {
         static void Main(string[] args)
         {
+            //Generic Tuple
             Tuple<int, int> result = DivideByTen(53);
+
+            //C# 7 Named Tuple
+            (int first, int second) namedTuple1 = (first: 10, second: 20);
+            namedTuple1.first++;
+            namedTuple1.second++;
+
+            (int c, int d) namedTuple2 = CreateNamedTupleMethod();
+
+            (int c, int d) namedTuple3 = CreateNamedTupleLocalFunction();
+
+            (int a, int b) CreateNamedTupleLocalFunction()
+            {
+                return (a: 10, b: 20);
+            };
         }
 
         static Tuple<int, int> DivideByTen(int input)
@@ -18,5 +33,7 @@ namespace Tuple
             int remainder = input % 10;
             return new Tuple<int, int>(quotient, remainder);
         }
+
+        static (int a, int b) CreateNamedTupleMethod() => (a: 10, b: 20);
     }
 }

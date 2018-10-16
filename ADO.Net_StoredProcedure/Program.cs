b6ab687d7b77b7142268e389f4f482a6a73a35cb
Malchazeen <malchazeen.edge@gmail.com>
2018-10-16 20:26:50 +0700

    Added a new example of ADO.Net Stored Procedure

--- ADO.Net_StoredProcedure/Program.cs
+++ ADO.Net_StoredProcedure/Program.cs
@@ -38,6 +38,32 @@ namespace ADO.Net_StoredProcedure
                         cmd.ExecuteNonQuery();
                         connection.Close();
                     }
+
+                    string procedureName2 = "_Proc";
+                    using (SqlCommand cmd = new SqlCommand(procedureName2, connection))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                        //Input Argument
+                        cmd.Parameters.Add(new SqlParameter { ParameterName = "@Param", DbType = System.Data.DbType.Int32, Value = 5, });
+
+                        //Ref Argument
+                        SqlParameter _refParam = new SqlParameter { ParameterName = "@RefParam", DbType = System.Data.DbType.Int32, Direction = System.Data.ParameterDirection.Output };
+                        cmd.Parameters.Add(_refParam);
+
+                        //Return Code Argument
+                        SqlParameter _returnCode = new SqlParameter { Direction = System.Data.ParameterDirection.ReturnValue };
+                        cmd.Parameters.Add(_returnCode);
+
+                        connection.Open();
+                        object scalarResult = cmd.ExecuteScalar();
+                        int? resultSet = null;
+                        if (scalarResult != null && scalarResult != DBNull.Value)
+                            resultSet = (int)scalarResult;
+                        int refParam = (int)_refParam.Value;
+                        int returnCode = (int)_returnCode.Value;
+                        connection.Close();
+                    }
                 }
             }
             catch (Exception e)

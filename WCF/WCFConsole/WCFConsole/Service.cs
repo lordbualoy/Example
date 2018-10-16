8bdf22cfb6c55d690b6db929f093001c4989897d
Malchazeen <malchazeen.edge@gmail.com>
2018-10-16 20:36:05 +0700

    Added WCFSoap and RESTful self-hosting with console app

---
+++ WCF/WCFConsole/WCFConsole/Service.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Runtime.Serialization;
+
+namespace WCFConsole
+{
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+        [OperationContract]
+        string Count();
+        [OperationContract]
+        Complex Complex(Complex complex);
+    }
+
+    [DataContract]
+    public class Complex
+    {
+        [DataMember]
+        public string Data { get; set; }
+    }
+
+    public static class Static
+    {
+        public static int Count { get; set; }
+    }
+
+    public class Calculator : ICalculator
+    {
+        static int count;
+
+        public double Add(double n1, double n2) => n1 + n2;
+
+        public double Divide(double n1, double n2) => n1 / n2;
+
+        public double Multiply(double n1, double n2) => n1 * n2;
+
+        public double Subtract(double n1, double n2) => n1 - n2;
+
+        public string Count() => $"{++count},{++Static.Count}";
+
+        public Complex Complex(Complex complex) => complex;
+
+        static void Main(string[] args)
+        {
+            // Step 1 Create a URI to serve as the base address.
+            Uri baseAddress = new Uri("http://localhost:8000/");
+
+            // Step 2 Create a ServiceHost instance
+            using (ServiceHost selfHost = new ServiceHost(typeof(Calculator), baseAddress))
+            {
+                try
+                {
+                    // Step 3 Add a service endpoint.
+                    selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "Calculator");
+
+                    // Step 4 Enable metadata exchange.
+                    selfHost.Description.Behaviors.Add(new ServiceMetadataBehavior
+                    {
+                        HttpGetEnabled = true,
+                    });
+
+                    // Step 5 Start the service.
+                    selfHost.Open();
+                    Console.WriteLine("The service is ready.");
+                    Console.WriteLine("Press <ENTER> to terminate service.");
+                    Console.WriteLine();
+                    Console.ReadLine();
+
+                    // Close the ServiceHostBase to shutdown the service.
+                    selfHost.Close();
+                }
+                catch (CommunicationException ce)
+                {
+                    Console.WriteLine("An exception occurred: {0}", ce.Message);
+                    selfHost.Abort();
+                }
+            }
+        }
+    }
+}
---
+++ WCF/WCFConsoleRest/WCFConsoleRest/Service.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFConsoleRest
+{
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [WebGet(UriTemplate = "Add?n1={n1}&n2={n2}")]
+        double Add(double n1, double n2);
+
+        [WebGet(UriTemplate = "Subtract?n1={n1}&n2={n2}")]
+        double Subtract(double n1, double n2);
+
+        [WebGet(UriTemplate = "Multiply?n1={n1}&n2={n2}")]
+        double Multiply(double n1, double n2);
+
+        [WebGet(UriTemplate = "Divide?n1={n1}&n2={n2}")]
+        double Divide(double n1, double n2);
+
+        [WebGet]
+        string Count();
+
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Bare
+            , Method = "POST"
+            , RequestFormat = WebMessageFormat.Json
+            , ResponseFormat = WebMessageFormat.Json
+            , UriTemplate = "Post")]
+        string Post(string body);
+
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Bare
+            , Method = "POST"
+            , RequestFormat = WebMessageFormat.Json
+            , ResponseFormat = WebMessageFormat.Json
+            , UriTemplate = "PostComplex")]
+        Complex PostComplex(Complex body);
+    }
+
+    [DataContract]
+    public class Complex
+    {
+        [DataMember]
+        public string Data { get; set; }
+    }
+    
+    public static class Static
+    {
+        public static int Count { get; set; }
+    }
+
+    public class Calculator : ICalculator
+    {
+        static int count;
+
+        public double Add(double n1, double n2) => n1 + n2;
+
+        public double Divide(double n1, double n2) => n1 / n2;
+
+        public double Multiply(double n1, double n2) => n1 * n2;
+
+        public double Subtract(double n1, double n2) => n1 - n2;
+
+        public string Count() => $"{++count},{++Static.Count}";
+
+        public string Post(string body) => body;
+
+        public Complex PostComplex(Complex body) => body;
+
+        static void Main(string[] args)
+        {
+            // Step 1 Create a URI to serve as the base address.
+            Uri baseAddress = new Uri("http://localhost:8000/");
+
+            // Step 2 Create a ServiceHost instance
+            using (WebServiceHost selfHost = new WebServiceHost(typeof(Calculator), baseAddress))
+            {
+                try
+                {
+                    // Step 3 Add a service endpoint.
+                    selfHost.AddServiceEndpoint(typeof(ICalculator), new WebHttpBinding(), "AdditionalPath");
+                    //for example http://localhost:8000/AdditionalPath/PostComplex
+
+                    // Step 4 Enable metadata exchange.
+                    selfHost.Description.Behaviors.Add(new ServiceMetadataBehavior
+                    {
+                        HttpGetEnabled = true,
+                    });
+
+                    // Step 5 Start the service.
+                    selfHost.Open();
+                    Console.WriteLine("The service is ready.");
+                    Console.WriteLine("Press <ENTER> to terminate service.");
+                    Console.WriteLine();
+                    Console.ReadLine();
+
+                    // Close the ServiceHostBase to shutdown the service.
+                    selfHost.Close();
+                }
+                catch (CommunicationException ce)
+                {
+                    Console.WriteLine("An exception occurred: {0}", ce.Message);
+                    selfHost.Abort();
+                }
+            }
+        }
+    }
+}

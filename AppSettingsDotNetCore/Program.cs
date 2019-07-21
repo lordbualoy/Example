b37de2e91b0a94d48d66e8454de46a0d1bf1bb09
Malchazeen <malchazeen.edge@gmail.com>
2019-07-21 16:52:37 +0700

    AppSettingsDotNetCore

---
+++ AppSettingsDotNetCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace AppSettingsDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            string sampleKey = config["SampleKey"];
+        }
+    }
+}
